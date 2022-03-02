0a2e95a250084855a4fc2c5882bc46bb2c647120
Jabuf <jason.buffet@gmail.com>
2022-03-02 13:14:24 +0100

    global: init a component and a main.css file

---
+++ src/components/SimpleButton.vue
@@ -0,0 +1,53 @@
+<template>
+  <button type="button" :class="classes" @click="onClick" :disabled="disabled">
+    {{ label }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "SimpleButton",
+
+  props: {
+    label: {
+      type: String,
+      required: true,
+      default: "",
+    },
+    color: {
+      type: String,
+      default: "primary",
+      validator: function (value) {
+        return ["primary", "warning", "danger"].indexOf(value) !== -1;
+      },
+    },
+    size: {
+      type: String,
+      default: "small",
+      validator: function (value) {
+        return ["small", "medium", "large"].indexOf(value) !== -1;
+      },
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
+
+  computed: {
+    classes() {
+      return {
+        "simple-button": true,
+        [`simple-button--${this.size}`]: true,
+        [`simple-button--${this.color}`]: true,
+      };
+    },
+  },
+
+  methods: {
+    onClick() {
+      this.$emit("onClick");
+    },
+  },
+};
+</script>
