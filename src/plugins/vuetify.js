74eec3ea4a8c020c426769d525b1413b9a121798
Jabuf <jason.buffet@gmail.com>
2022-03-09 16:53:01 +0100

    config(css): add tailwind colors to vuetify (*o*)

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,18 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify/lib/framework'
 import '@mdi/font/css/materialdesignicons.css'
+import formattedColors from "@/plugins/tailwindcolors-into-vuetify"
+import { theme } from '../../tailwind.config'
 
 Vue.use(Vuetify)
 
-export default new Vuetify({})
+export default new Vuetify({
+    theme: {
+        themes: {
+            light: {
+                ...formattedColors,
+                primary: theme.extend.colors['alias-primary']['500']
+            }
+        }
+    }
+})
