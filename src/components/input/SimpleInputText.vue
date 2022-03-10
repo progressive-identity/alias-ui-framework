89b71c63d768fc5e0f7c279e223d2280233f9447
Jabuf <jason.buffet@gmail.com>
2022-03-10 11:48:44 +0100

    simple-input-text: add component

---
+++ src/components/input/SimpleInputText.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-text-field
+      :prepend-icon="iconLeft"
+      :append-icon="iconRight"
+      :counter="maxLength"
+      :label="label"
+      :disabled="disabled"
+      :required="required"
+      v-bind="[getType, hideHint]"
+  >
+  </v-text-field>
+</template>
+
+<script>
+export default {
+  name: "SimpleInput",
+
+  props: {
+    label: {
+      type: String,
+      required: false,
+      default: '',
+    },
+    maxLength: {
+      type: String,
+      required: false,
+      default: '',
+    },
+    // List of available icons here https://materialdesignicons.com/
+    iconLeft: {
+      type: String,
+      default: ''
+    },
+    iconRight: {
+      type: String,
+      default: ''
+    },
+    required: {
+      type: Boolean,
+      default: false
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  },
+
+  computed: {
+    getType() {
+      return this.label.length > 0 ? {['outlined']: true} : {['solo']: true}
+    },
+    hideHint() {
+      return this.maxLength === '' ? {['hide-details']: true} : {}
+    }
+  },
+}
+</script>
