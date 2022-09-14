3faf391b56696873908ef782f18f049754b98bf7
Jules Prévost <jprevostdev@gmail.com>
2022-09-14 22:47:55 +0200

    test with enum

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import "tailwindcss/tailwind.css";
 
 import AliasButton from "./components/atomics/AliasButton.vue";
+import Test from "./components/atomics/Test.vue";
 import * as Colors from "./AliasColors";
 
-export { AliasButton, Colors };
+export { AliasButton, Colors, Test };
