d2f857dab86a895773e61967f7cd8bbaafa94657
Jules Prévost <jprevostdev@gmail.com>
2022-09-15 10:00:16 +0200

    add AliasPresets to ViteBuild

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,7 @@ import "tailwindcss/tailwind.css";
 import AliasButton from "./components/atomics/AliasButton.vue";
 import Test from "./components/atomics/Test.vue";
 import * as Colors from "./AliasColors";
+// @ts-ignore
+import AliasPresets from "../tailwind-presets.cjs";
 
-export { AliasButton, Colors, Test };
+export { AliasButton, Colors, Test, AliasPresets };
