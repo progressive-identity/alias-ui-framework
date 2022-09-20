de796943ef2fbf25002ff4ca32fc76889dd4e21c
Djoudi21 <abdelkrim@gdpr.dev>
2022-09-20 11:51:59 +0200

    style modifications based on new linter setting

--- src/components/atomics/ASwitch.stories.ts
+++ src/components/atomics/ASwitch.stories.ts
@@ -1,22 +1,26 @@
 import ASwitch from './ASwitch.vue'
 import { Meta, StoryFn } from '@storybook/vue3'
 import { ArgTypeUtils } from '../../../utils/ArgTypeUtils'
-import { ColorNames } from '../../AliasColors'
+import { AliasColorNames } from '../../AliasColors'
 
 export default {
-    title: 'Atomics/ASwitch',
-    component: ASwitch,
-    argTypes: {
-        type: ArgTypeUtils.buildArgTypeFromEnumProp(ASwitch, 'type', ColorNames)
-    }
+  title: 'Atomics/ASwitch',
+  component: ASwitch,
+  argTypes: {
+    type: ArgTypeUtils.buildArgTypeFromEnumProp(
+      ASwitch,
+      'type',
+      AliasColorNames
+    ),
+  },
 } as Meta<typeof ASwitch>
 
 const Template: StoryFn<typeof ASwitch> = (args) => ({
-    components: { ASwitch },
-    setup() {
-        return { args }
-    },
-    template: '<ASwitch v-bind="args" />'
+  components: { ASwitch },
+  setup() {
+    return { args }
+  },
+  template: '<ASwitch v-bind="args" />',
 })
 
 export const Default = Template.bind({})
--- src/components/atomics/ASwitch.vue
+++ src/components/atomics/ASwitch.vue
@@ -1,59 +1,56 @@
 <template>
-  <SwitchGroup as="div" class="alias-flex alias-items-center alias-pt-2 alias-pb-2">
-    <SwitchLabel
-        v-if="label"
-        as="span"
-        class="alias-mr-3"
-    >
-      <span class="alias-block alias-text-xs alias-font-medium alias-text-gray-900">{{ label }}</span>
+  <SwitchGroup
+    as="div"
+    class="alias-flex alias-items-center alias-pt-2 alias-pb-2"
+  >
+    <SwitchLabel v-if="label" as="span" class="alias-mr-3">
+      <span
+        class="alias-block alias-text-xs alias-font-medium alias-text-gray-900"
+        >{{ label }}</span
+      >
     </SwitchLabel>
     <Switch
-        v-model="checked"
-        @update:checked="update"
-        :class="[checked ? 'alias-bg-primary-500' : 'alias-bg-gray-200']"
-        class="alias-relative alias-inline-flex alias-w-[74px] alias-shrink-0 alias-cursor-pointer alias-rounded-full
-        alias-border-2 alias-border-transparent alias-transition-colors alias-duration-200 alias-ease-in-out
-        focus:alias-outline-none focus:alias-ring-2 focus:alias-ring-offset-2 focus:alias-ring-primary-500"
+      v-model="checked"
+      @update:checked="update"
+      :class="[checked ? 'alias-bg-primary-500' : 'alias-bg-gray-200']"
+      class="alias-relative alias-inline-flex alias-w-[74px] alias-shrink-0 alias-cursor-pointer alias-rounded-full alias-border-2 alias-border-transparent alias-transition-colors alias-duration-200 alias-ease-in-out focus:alias-outline-none focus:alias-ring-2 focus:alias-ring-primary-500 focus:alias-ring-offset-2"
     >
-      <span aria-hidden="true"
-          :class="[checked ? 'alias-translate-x-9' : 'alias-translate-x-0']"
-          class="alias-pointer-events-none alias-inline-block alias-h-[34px] alias-w-[34px]
-          alias-transform alias-rounded-full alias-bg-white alias-shadow alias-ring-0 alias-transition
-          alias-duration-200 alias-ease-in-out"
-    />
+      <span
+        aria-hidden="true"
+        :class="[checked ? 'alias-translate-x-9' : 'alias-translate-x-0']"
+        class="alias-pointer-events-none alias-inline-block alias-h-[34px] alias-w-[34px] alias-transform alias-rounded-full alias-bg-white alias-shadow alias-ring-0 alias-transition alias-duration-200 alias-ease-in-out"
+      />
     </Switch>
   </SwitchGroup>
 </template>
 
 <script setup lang="ts">
-import {Switch, SwitchGroup, SwitchLabel} from '@headlessui/vue'
+import { Switch, SwitchGroup, SwitchLabel } from '@headlessui/vue'
 import { PropType, toRefs } from 'vue'
-import { ColorNames } from '../../AliasColors'
+import { AliasColorNames } from '../../AliasColors'
 
 const props = defineProps({
-  type: { type: String as PropType<ColorNames>, required: true },
+  type: { type: String as PropType<AliasColorNames>, required: true },
   /**
    * The text displayed on the left of the switch
    */
   label: {
     type: String,
-    default: ''
+    default: '',
   },
   /**
    * The value of the switch, if it has been checked or not
    */
   checked: {
     type: Boolean,
-    required: true
-  }
+    required: true,
+  },
 })
-const {checked} = toRefs(props)
-
+const { checked } = toRefs(props)
 
 const emits = defineEmits(['update:checked'])
 
 const update = (value: any) => {
   emits('update:checked', value)
 }
-
 </script>
