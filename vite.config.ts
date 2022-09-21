5ceaaee6104d75010de2b593ebab38dd09d5a87c
Jules Prévost <jprevostdev@gmail.com>
2022-09-21 19:14:44 +0200

    (config): rework vite and ts config

--- vite.config.ts
+++ vite.config.ts
@@ -3,28 +3,28 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import dts from 'vite-plugin-dts'
 
-
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
       name: 'alias-ui-framework',
-      fileName: (format) => `alias-ui-framework.${format}.js`
+      fileName: (format) => `alias-ui-framework.${format}.js`,
     },
     outDir: path.resolve(__dirname, 'dist/lib'),
     rollupOptions: {
       external: ['vue'],
       output: {
         globals: {
-          vue: 'Vue'
-        }
-      }
-    }
+          vue: 'Vue',
+        },
+      },
+    },
   },
-  plugins: [vue(), dts({
-      outputDir: path.resolve(__dirname, 'dist/types')
-  }
-  )]
+  plugins: [
+    vue(),
+    dts({
+      outputDir: path.resolve(__dirname, 'dist/types'),
+    }),
+  ],
 })
-
