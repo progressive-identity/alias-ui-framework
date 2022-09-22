bed209f52b76782b45802af07f43ed6468e12da8
Djoudi21 <abdelkrim@gdpr.dev>
2022-09-22 11:31:23 +0200

    auf-21: delete useless code

--- src/components/atomics/ACheckbox.stories.ts
+++ src/components/atomics/ACheckbox.stories.ts
@@ -1,40 +1,10 @@
 import ACheckbox from './ACheckbox.vue'
 import { Meta, StoryFn } from '@storybook/vue3'
-// import { ArgTypeUtils } from '../../../utils/ArgTypeUtils'
-// import { AliasColorNames } from '../../AliasColors'
-// import { ButtonSizes } from '../../AliasSizes'
-// const colorPropName = ArgTypeUtils.propNameToString(ACheckbox, 'color')
-// const sizePropName = ArgTypeUtils.propNameToString(ACheckbox, 'size')
-// const iconPathPropName = ArgTypeUtils.propNameToString(ACheckbox, 'iconPath')
+
 export default {
   title: 'Atomics/ACheckbox',
   component: ACheckbox,
-  argTypes: {
-    // list: {
-    //   control: {
-    //     type: 'select',
-    //   },
-    //   options: ['first', 'second'],
-    //   mapping: {
-    //     first: [{ name: 'toto' }, { name: 'tutu' }],
-    //     second: { name: 'toto' },
-    //   },
-    // },
-    //
-    // modelValue: {
-    //   control: {
-    //     type: 'array',
-    //     labels: {
-    //       first: 'First toto',
-    //       second: 'Second toto',
-    //     },
-    //   },
-    //   options: ['first'],
-    //   mapping: {
-    //     first: [{ name: 'toto' }, { name: 'tutu' }],
-    //   },
-    // },
-  },
+  argTypes: {},
 } as Meta<typeof ACheckbox>
 
 const Template: StoryFn<typeof ACheckbox> = (args) => ({
@@ -42,8 +12,7 @@ const Template: StoryFn<typeof ACheckbox> = (args) => ({
   setup() {
     return { args }
   },
-  template:
-    "<ACheckbox v-bind=\"args\" :list=\"[{ name: 'toto' }, { name: 'tutu' }]\" :model-value=\"[{ name: 'toto' }, { name: 'tutu' }]\" />",
+  template: '<ACheckbox v-bind="args" />',
 })
 
 export const Default = Template.bind({})
