6b77fb042ae36b299e19e70cbaee25a2aac96ebd
Jules Prévost <jprevostdev@gmail.com>
2022-09-22 09:26:06 +0200

    new travis config

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
-      name: 'alias-ui-framework',
+      name: '@alias-code-is-law/alias-ui-framework',
       fileName: (format) => `alias-ui-framework.${format}.js`,
     },
     outDir: path.resolve(__dirname, 'dist/lib'),
