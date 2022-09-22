2bde1330de2e61ab5cf672e4d5f8f3a7ac5edf26
Djoudi21 <abdelkrim@gdpr.dev>
2022-09-22 21:54:29 +0200

    auf-33: add args and delete commented code

--- src/components/atomics/AFooter.stories.ts
+++ src/components/atomics/AFooter.stories.ts
@@ -1,17 +1,13 @@
 import AFooter from './AFooter.vue'
 import { Meta, StoryFn } from '@storybook/vue3'
-// eslint-disable-next-line no-unused-vars
-import { externalLinks } from '../../../utils/constants/ExternalLinksConstants'
-// eslint-disable-next-line no-unused-vars,@typescript-eslint/no-unused-vars
-import { pagesRedirection } from '../../../utils/constants/LegalTextConstants'
 export default {
   title: 'Atomics/AFooter',
   component: AFooter,
   argTypes: {},
-  // args: {
-  //   externalLinks: [],
-  //   pagesRedirection: [],
-  // },
+  args: {
+    externalLinks: [],
+    internalLinks: [],
+  },
 } as Meta<typeof AFooter>
 
 const Template: StoryFn<typeof AFooter> = (args) => ({
