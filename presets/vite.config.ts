2d668a61da65ac8cba5e5eadc00841694a67404c
Jules Prévost <jprevostdev@gmail.com>
2022-09-22 10:48:38 +0200

    add travis config for presets

--- presets/vite.config.ts
+++ presets/vite.config.ts
@@ -6,10 +6,10 @@ export default defineConfig({
   build: {
     lib: {
       entry: path.resolve(__dirname, 'tailwind-presets.cjs'),
-      name: 'alias-ui-framework-presets',
-      fileName: (format) => `alias-ui-framework.${format}.js`
+      name: '@alias-code-is-law/alias-ui-framework-presets',
+      formats: ['cjs'],
+      fileName: (format) => `alias-ui-framework-presets.${format}`,
     },
-    outDir: path.resolve(__dirname, 'dist/lib')
-  }
+    outDir: path.resolve(__dirname, 'dist/lib'),
+  },
 })
-
