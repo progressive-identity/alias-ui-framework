1446f781ccdb1ce3b13252121f3d0135b63e329d
Djoudi21 <abdelkrim@gdpr.dev>
2022-09-22 11:30:51 +0200

    auf-21: implement checkbox component

--- src/components/atomics/ACheckbox.vue
+++ src/components/atomics/ACheckbox.vue
@@ -1,38 +1,25 @@
 <template>
   <div class="alias-block alias-py-2">
-    <label class="u-label">{{ label }} </label>
     <p
-      v-if="!state.valid"
+      v-if="!state.valid && !state.checked"
       v-for="error in state.errors"
       class="alias-mt-1 alias-text-sm alias-font-normal alias-text-red-600"
       id="error"
     >
       {{ error }}
     </p>
-    <fieldset class="alias-space-y-5 alias-py-2">
-      <div
-        v-for="(element, index) in list"
-        class="alias-relative alias-flex alias-items-start"
-      >
-        <div class="alias-flex alias-h-5 alias-items-center">
-          <input
-            :id="getName(element)"
-            :name="getName(element)"
-            v-model="state.checkBoxes[index]"
-            type="checkbox"
-            @click="modifyList(element, index)"
-            @change="validate(state, props, modelValue)"
-            class="alias-h-4 alias-w-4 alias-rounded alias-border-gray-300 alias-text-primary-600 focus:alias-ring-primary-500"
-          />
-          <div class="alias-ml-3 alias-text-sm">
-            <label :for="getName(element)" class="text-sm">{{
-              getName(element)
-            }}</label>
-          </div>
-        </div>
+
+    <div class="alias-flex alias-h-5 alias-items-center">
+      <input
+        v-model="state.checked"
+        type="checkbox"
+        @change="update"
+        class="alias-h-4 alias-w-4 alias-rounded alias-border-gray-300 alias-text-primary-600 focus:alias-ring-primary-500"
+      />
+      <div class="alias-ml-3 alias-text-sm">
+        <label class="alias-text-sm">{{ label }}</label>
       </div>
-    </fieldset>
-    {{ modelValue }}
+    </div>
   </div>
 </template>
 <script setup>
@@ -48,22 +35,13 @@ const props = defineProps({
     default: '',
   },
   /**
-   * The value of the label associated with the checkbox
+   * The value of the title associated with the checkbox
    */
-  modelValue: {
-    type: Array,
-    required: true,
+  title: {
+    type: String,
+    default: '',
   },
-  /**
-   * The list of elements to display as a checkbox
-   */
-  list: {
-    type: Array,
-    required: true,
-  },
-  /**
-   * The value of the label associated with the checkbox
-   */
+  // TODO: add the doc for this prop
   namePath: {
     type: String,
     default: null,
@@ -73,46 +51,20 @@ const props = defineProps({
    */
   required: {
     type: Boolean,
-    default: false,
+    default: true,
   },
 })
-const { checked } = toRefs(props)
 
 const state = reactive({
   valid: true,
   errors: [],
-  checkBoxes: Array(props.list.length),
+  checked: false,
 })
 
 const emits = defineEmits(['change:checked'])
 
-const update = (value) => {
-  emits('change:checked', value)
-}
-
-function modifyList(element, index) {
-  console.log('EL', element)
-  console.log('I', index)
-  console.log('!state.checkBoxes[index]', !state.checkBoxes[index])
-  console.log('state.checkBoxes', state.checkBoxes)
-  console.log('state.checkBoxes[index]', state.checkBoxes[index])
-
-  if (!state.checkBoxes[index]) {
-    console.log('MODELVALUE', props.modelValue)
-    props.modelValue.push(element)
-  } else {
-    const existingIndex = props.modelValue.findIndex(
-      (e) => getName(e) === getName(element)
-    )
-    props.modelValue.splice(existingIndex, 1)
-  }
-}
-
-function getName(element) {
-  if (props.namePath) {
-    return element[props.namePath].name
-  } else {
-    return element.name
-  }
+const update = () => {
+  validate(state, props)
+  emits('change:checked', state.checked)
 }
 </script>
